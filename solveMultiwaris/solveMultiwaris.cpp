50279353d77115dda709bbc35dd267ff7998faf7
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2024-05-27 10:55:36 +0700

    add class waris(budi) dari class pekerja dan pelajar

--- solveMultiwaris/solveMultiwaris.cpp
+++ solveMultiwaris/solveMultiwaris.cpp
@@ -28,6 +28,14 @@ public:
     }
 };
 
+class budi :public pekerja, public pelajar {
+public:
+    budi(int pUmur) :pekerja(pUmur), pelajar(pUmur)
+    {
+        cout << "budi dibuat\n" << endl;
+    }
+};
+
 int main()
 {
     
